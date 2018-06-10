3b8b5d09458661f3849bceadcbb0de7d7dd5bb7d
Castracani <willmyers993@gmail.com>
2018-06-09 20:14:01 -0400

    finished homework with pseudocode included in files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,15 +26,13 @@ function startUp() {
 
 //takes the valuesArray and randomizes the order, assigns this to local variable crystalValues//
 crystalValues = valuesArray.sort( function() { return 0.5 - Math.random() } );
-console.log(crystalValues);
 
 //grabs the first four numbers of the crystalValues array//
 pulledCrystalValues = crystalValues.slice(0, 4);
-console.log(pulledCrystalValues);
 
 //converts the values of the crystalValues array from strings into integers//
-pulledCrystalIntegers = pulledCrystalValues.map(Number);
-console.log(pulledCrystalIntegers[0]);
+//pulledCrystalIntegers = pulledCrystalValues.map(Number);
+//console.log(pulledCrystalIntegers[0]);
 
 // do I need to create a parameter to manipulate the score for the on-click function? Not sure//
 
@@ -45,10 +43,10 @@ but I'm unsure how to implement it... so I'll simply type out everything.
 for (i = 0; i < pulledCrystalIntegers.length; i++) {
 } to begin... but don't know where to go from there?*/
 
- $("#crystal0").attr("data-crystalValue",pulledCrystalIntegers[0]);
- $("#crystal1").attr("data-crystalValue", pulledCrystalIntegers[1]);
- $("#crystal2").attr("data-crystalValue", pulledCrystalIntegers[2]);
- $("#crystal3").attr("data-crystalValue", pulledCrystalIntegers[3]);
+ $("#crystal0").attr("data-crystalValue",pulledCrystalValues[0]);
+ $("#crystal1").attr("data-crystalValue", pulledCrystalValues[1]);
+ $("#crystal2").attr("data-crystalValue", pulledCrystalValues[2]);
+ $("#crystal3").attr("data-crystalValue", pulledCrystalValues[3]);
 
 
 //assign a variable to generate a random number to be determined as the winning number (19 - 120)//
@@ -72,7 +70,6 @@ $("#score").html(currentScore);
  
 //create a function that runs when clicking any of the crystal images//
 $(".crystalImg").on("click", function () { 
-    console.log("hey");
     /*taking the winningNumber variable from the startUp function
     and passing it to this function*/
     // winningNumber = finalScore;
@@ -92,7 +89,7 @@ $(".crystalImg").on("click", function () {
     if (currentScore === winningNumber) {
         wins++;
         $("#wins").html(wins);
-        $("#gameStateText").html("You won!!");
+        $("#gameStateText").html("You won!! <br />");
         startUp();
     }
     
@@ -102,7 +99,7 @@ $(".crystalImg").on("click", function () {
     else if ( currentScore > winningNumber) {
         losses++;
         $("#losses").html(losses);
-        $("#gameStateText").html("You lost!!");
+        $("#gameStateText").html("You lost!! <br />");
         startUp();
     }
 
--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -12,3 +12,77 @@ console.log(confusion);
 //converts the values of the crystalValues array from strings into integers//
 confusionIntegers = confusion.map(Number);
 console.log(confusionIntegers);
+
+//assigns the integer values previously gotten to the crystal images//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Global Variables//
+
+wins = 0;
+losses = 0;
+var valuesArray = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+
+
+
+
+
+//FUNCTONS//
+
+
+//start-up game function//
+***create function with name of locally defined variable in parentheses***
+/* function startUp(randomValues, randomWinNumber)*/
+
+
+generates 4 random numbers from a pre-determined list (1 - 12) [for loop?]
+
+assign the four gotten numbers to a locally defined variable
+assigns these numbers to the crystals (first crystal gets first number, etc.)
+
+assign a variable as an operation to generate a random number to be determined as the winning number (19 - 120)
+insert that number into the #RandomNumDiv container
+
+set number in #scoreDiv to zero
+
+
+
+
+
+//on-click function//
+defined for clicking the four crystals
+clicking the crystal will increase the points total in the #scoreDiv
+conditionals from here
+
+if (point value equals the randomly-generated winning number)
+increase wins variable by 1    
+insert text into #gameStateText saying "You won!!"
+run startUp function
+
+else if ([point value exceeds the randomly-generated winning number])
+    increase losses variable by 1
+    insert text into #gameStateText saying "You Lose!!"
+    run startUp function
+
+
+
+
